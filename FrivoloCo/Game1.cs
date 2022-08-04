b5d9cd0ca73379f14affc973c9b21f9ee29e0b0e
Royce551 <poohwie2005@gmail.com>
2022-08-04 08:11:04 -0500

    Start on FCO

---
+++ FrivoloCo/Game1.cs
@@ -0,0 +1,40 @@
+﻿using FrivoloCo.Screens.Menu;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Water;
+
+namespace FrivoloCo
+{
+    public class Game1 : WaterGame
+    {
+        public override string ProjectName => "FrivoloCo: The Game";
+
+        public Game1() : base()
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            Screen.ChangeScreen(new MenuScreen());
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // TODO: use this.Content to load your game content here
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FrivoloCo/Screens/Menu/MenuScreen.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Water.Graphics.Controls;
+using Water.Graphics.Screens;
+
+namespace FrivoloCo.Screens.Menu
+{
+    public class MenuScreen : Screen
+    {
+        public override void Deinitialize()
+        {
+            
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+            var tb = new TextBlock(new(0, 0, 500, 100), Game.Fonts.Get("Assets/Fonts/parisienne-regular.ttf", 50), "Welcome to FrivoloCo", Color.White)
+            {
+                Layout = Water.Graphics.Layout.Center
+            };
+            AddChild(Game.AddObject(tb));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
