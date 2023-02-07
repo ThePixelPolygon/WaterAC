4bd0b0899ea48868caf805eb0b9255a921430b77
The Pixel Polygon <ThePixelPolygon@users.noreply.github.com>
2023-02-07 00:37:15 -0500

    Initial credit overlay code

--- FrivoloCo/Screens/CreditOverlay.cs
+++ FrivoloCo/Screens/CreditOverlay.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Water.Graphics;
 using Water.Graphics.Screens;
 using Water.Graphics.Controls;
 using Water.Graphics.Containers;
@@ -11,6 +12,8 @@ public class CreditOverlay : Screen
 {
     private arcadeShim acShim = arcadeShim.getInstance();
     private UniformStackContainer stackContainer;
+
+    private TextBlock creditDisplay;
     private void Input_KeyDown(object sender, Water.Input.KeyEventArgs e)
     {
         if (e.Key == Keys.F3)
@@ -26,6 +29,9 @@ public class CreditOverlay : Screen
             RelativePosition = new Rectangle(0, 0, 200, 60),
             Layout = Water.Graphics.Layout.DockBottom
         };
+
+        creditDisplay = new TextBlock(new(0, 0, 100, 60),
+            Game.Fonts.Get("Assets/IBMPLEXSANS-MEDIUM.TTF", 36), "", Color.Black);
     }
 
     public override void Deinitialize()
@@ -35,7 +41,18 @@ public class CreditOverlay : Screen
 
     public override void Update(GameTime gameTime)
     {
-        
+        int coinsPerCredit = acShim.CoinsPerCredit;
+        int coinsInserted = acShim.CoinsInserted;
+        int credits = acShim.Credits;
+
+        if (coinsPerCredit == 0)
+        {
+            creditDisplay.Text = "FREE PLAY";
+        }
+        else
+        {
+            creditDisplay.Text = $"Credits: {credits} ({coinsInserted}/{coinsPerCredit})";
+        }
     }
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, GraphicsDevice graphicsDevice)
